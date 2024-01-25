b77e7d2c00499a6bcb726adcaed635411ba29e41
richardd3ng <richard.deng@duke.edu>
2024-01-25 15:28:25 -0500

    refactored file structure for UI and routes

--- pages/index.tsx
+++ pages/index.tsx
@@ -1,17 +1,16 @@
 import Head from "next/head";
 import Image from "next/image";
 import { Inter } from "next/font/google";
-import NavBar from "./components/nav-bar";
-import Stack from "@mui/joy/Stack";
-import ExperimentList from "./ExperimentList/experiment-list";
+import NavBar from "../components/shared/nav-bar";
+import ExperimentList from "./experiment-list";
 
 const inter = Inter({ subsets: ["latin"] });
 
 export default function Home() {
-  return (
-    <Stack spacing={2}>
-      <NavBar />
-      <ExperimentList />
-    </Stack>
-  );
+    // return (
+    //     <Stack spacing={2}>
+    //         <NavBar />
+    //         <ExperimentList />
+    //     </Stack>
+    // );
 }
