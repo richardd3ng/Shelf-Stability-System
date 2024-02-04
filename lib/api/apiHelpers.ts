5b3d8092035af5319be5c8a9f8a63333b589c94a
richardd3ng <rld39@duke.edu>
2024-02-03 23:41:01 -0500

    add getErrorMessage() back

--- lib/api/apiHelpers.ts
+++ lib/api/apiHelpers.ts
@@ -1,12 +1,20 @@
 import { NextApiRequest } from "next";
 import { INVALID_EXPERIMENT_ID } from "../hooks/useExperimentId";
 
-export const getExperimentID = (req: NextApiRequest) : number => {
-    return req.query.experimentId? Number(req.query.experimentId.toString()) : INVALID_EXPERIMENT_ID;
-}
-
-
+export const getExperimentID = (req: NextApiRequest): number => {
+    return req.query.experimentId
+        ? Number(req.query.experimentId.toString())
+        : INVALID_EXPERIMENT_ID;
+};
 export const INVALID_ASSAY_ID = -1;
-export const getAssayID = (req: NextApiRequest) : number => {
-    return req.query.assayId? Number(req.query.assayId.toString()) : INVALID_ASSAY_ID;
+
+export const getAssayID = (req: NextApiRequest): number => {
+    return req.query.assayId
+        ? Number(req.query.assayId.toString())
+        : INVALID_ASSAY_ID;
+};
+
+export function getErrorMessage(error: unknown): string {
+    if (error instanceof Error) return error.message;
+    return String(error);
 }
