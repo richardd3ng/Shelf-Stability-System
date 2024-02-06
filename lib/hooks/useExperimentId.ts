6348f14e2e25ee65cb9c4eafcf5cb0763ddf7e4d
Keith Cressman <77999322+KeithCressman@users.noreply.github.com>
2024-02-06 15:26:16 -0500

    Delete lib/hooks/useExperimentId.ts

--- lib/hooks/useExperimentId.ts
+++
@@ -1,7 +0,0 @@
-import { useRouter } from "next/router";
-export const INVALID_EXPERIMENT_ID = -1;
-export const useExperimentId = () : number => {
-    const router = useRouter();
-    const experimentId = router.query.experimentId ? Number(router.query.experimentId.toString()) : INVALID_EXPERIMENT_ID;
-    return experimentId;
-}
