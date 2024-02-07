62e25848d3e34ea2f768ad70528b89938892f9fb
richardd3ng <rld39@duke.edu>
2024-02-07 18:14:56 -0500

    fix password change bug

--- lib/controllers/authController.ts
+++ lib/controllers/authController.ts
@@ -1,28 +1,39 @@
+import { ApiError } from "next/dist/server/api-utils";
+
 interface UpdatePasswordArgs {
-    newPassword : string;
-    oldPassword : string;
-}
-export const updatePasswordThroughAPI = async (newPasswordInfo : UpdatePasswordArgs) : Promise<UpdatePasswordArgs> => {
-    const apiResponse = await fetch("/api/auth/updatePassword", {
-        method: "POST",
-        body : JSON.stringify( {newPassword : newPasswordInfo.newPassword, oldPassword : newPasswordInfo.oldPassword}),
-        headers: {
-            "Content-Type": "application/json",
-        },
-    });
-    if (apiResponse.status > 300) {
-        throw new Error("An error occurred");
-    }
-    return newPasswordInfo;
+    newPassword: string;
+    oldPassword: string;
 }
 
+export const updatePasswordThroughAPI = async (
+    newPasswordInfo: UpdatePasswordArgs
+): Promise<UpdatePasswordArgs> => {
+    const apiResponse = await fetch("/api/auth/updatePassword", {
+        method: "POST",
+        body: JSON.stringify({
+            newPassword: newPasswordInfo.newPassword,
+            oldPassword: newPasswordInfo.oldPassword,
+        }),
+        headers: {
+            "Content-Type": "application/json",
+        },
+    });
+    const resJson = await apiResponse.json();
+    if (apiResponse.status > 300) {
+        throw new ApiError(apiResponse.status, resJson.message);
+    }
+    return newPasswordInfo;
+};
+
 interface SetPasswordOnSetupArgs {
-    newPassword : string;
+    newPassword: string;
 }
-export const setPasswordOnSetupThroughAPI = async (newPasswordInfo : SetPasswordOnSetupArgs) : Promise<SetPasswordOnSetupArgs> => {
+export const setPasswordOnSetupThroughAPI = async (
+    newPasswordInfo: SetPasswordOnSetupArgs
+): Promise<SetPasswordOnSetupArgs> => {
     const apiResponse = await fetch("/api/auth/setPasswordOnSetup", {
         method: "POST",
-        body : JSON.stringify( {newPassword : newPasswordInfo.newPassword}),
+        body: JSON.stringify({ newPassword: newPasswordInfo.newPassword }),
         headers: {
             "Content-Type": "application/json",
         },
@@ -31,4 +42,4 @@ export const setPasswordOnSetupThroughAPI = async (newPasswordInfo : SetPassword
         throw new Error("An error occurred");
     }
     return newPasswordInfo;
-}
+};
