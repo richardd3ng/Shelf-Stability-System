2eff53112e309651aa2f7bd5b14e54535ded5a2c
Brokemia <37253216+Brokemia@users.noreply.github.com>
2024-02-07 22:28:01 -0500

    Page titles

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -4,25 +4,25 @@ import { AdapterDayjs } from "@mui/x-date-pickers/AdapterDayjs";
 import type { AppProps } from "next/app";
 import { QueryClientProvider, QueryClient } from "@tanstack/react-query";
 import { AlertProvider } from "@/lib/context/alert-context";
-import { Metadata } from "next";
-
-export const metadata: Metadata = {
-    icons: {
-        icon: '/favicon.ico',
-    }
-};
+import Head from "next/head";
 
 const queryClient = new QueryClient();
 export default function App({ Component, pageProps }: AppProps) {
     //const queryClient = new QueryClient();
 
     return (
-        <QueryClientProvider client={queryClient}>
-            <AlertProvider>
-                <LocalizationProvider dateAdapter={AdapterDayjs}>
-                    <Component {...pageProps} />
-                </ LocalizationProvider>
-            </AlertProvider>
-        </QueryClientProvider>
+        <>
+            <Head>
+                <link rel="icon" href="/favicon.ico" />
+                <title>Shelf Stability System</title>
+            </Head>
+            <QueryClientProvider client={queryClient}>
+                <AlertProvider>
+                    <LocalizationProvider dateAdapter={AdapterDayjs}>
+                        <Component {...pageProps} />
+                    </ LocalizationProvider>
+                </AlertProvider>
+            </QueryClientProvider>
+        </>
     );
 }
