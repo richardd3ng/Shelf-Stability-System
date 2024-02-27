fb5a6e5807e775255d73be010c721a04b488538a
Keith Cressman <keith.cressman@duke.edu>
2024-02-27 01:48:12 -0500

    new folder for db operations

---
+++ lib/api/dbOperations/assayResultOperations.ts
@@ -0,0 +1,21 @@
+import { AssayCreationArgs, AssayResultCreationArgs } from "../../controllers/types";
+import {db} from "../db";
+
+export const createAssayResults = async (assayResults : AssayResultCreationArgs[]) => {
+    await db.assayResult.createMany({
+        data : assayResults.map((assayResult) => {
+            return {
+                assayId : assayResult.assayId,
+                result : assayResult.result,
+                comment : assayResult.comment,
+                last_editor : assayResult.last_editor
+            };
+        })
+            
+    });
+    
+
+
+}
+
+
