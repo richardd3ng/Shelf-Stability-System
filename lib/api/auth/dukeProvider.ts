e87f22985922435d31096b56637485ca9569ffb2
Brokemia <37253216+Brokemia@users.noreply.github.com>
2024-03-04 17:33:09 -0500

    Some Oauth tweaks

--- lib/api/auth/dukeProvider.ts
+++ lib/api/auth/dukeProvider.ts
@@ -1,18 +1,28 @@
-export default function DukeProvider() {
+import { OAuthConfig } from "next-auth/providers/oauth"
+
+export interface DukeProfile {
+    
+}
+
+// Redirect will be http://localhost:3000/api/auth/callback/duke for local
+export default function DukeProvider(): OAuthConfig<DukeProfile> {
     return {
         id: "duke",
         name: "Duke",
         type: "oauth",
-        wellKnown: "https://accounts.google.com/.well-known/openid-configuration", // TODO
-        authorization: { params: { scope: "openid email profile" } }, // TODO
+        wellKnown: "https://oauth.oit.duke.edu/oidc/.well-known/openid-configuration",
+        authorization: { params: { scope: "openid email profile" } },
         idToken: true, // TODO
         checks: ["pkce", "state"], // TODO
-        profile(profile) { // TODO
+        clientId: process.env.DUKE_CLIENT_ID,
+        clientSecret: process.env.DUKE_CLIENT_SECRET,
+        profile(profile) {
+            // TODO get details we need from profile
             return {
-                id: profile.sub,
-                name: profile.name,
-                email: profile.email,
-                image: profile.picture,
+                id: "",
+                name: "",
+                email: "",
+                image: null
             }
         },
     }
