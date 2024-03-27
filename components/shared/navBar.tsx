2e22b56126247a15979de3498362924c5843713e
richardd3ng <rld39@duke.edu>
2024-03-27 13:26:22 -0400

    clicking on title brings you back to home

--- components/shared/navBar.tsx
+++ components/shared/navBar.tsx
@@ -1,5 +1,5 @@
 import React, { useContext } from "react";
-import { Button, AppBar, Toolbar, Typography } from "@mui/material";
+import { AppBar, Button, Link, Toolbar, Typography } from "@mui/material";
 import { useRouter } from "next/router";
 import { CurrentUserContext } from "@/lib/context/users/currentUserContext";
 
@@ -8,13 +8,19 @@ interface NavBarButtonProps {
     onClick: () => void;
     hidden?: boolean;
 }
-const NavBarButton: React.FC<NavBarButtonProps> = (props: NavBarButtonProps) => {
+const NavBarButton: React.FC<NavBarButtonProps> = (
+    props: NavBarButtonProps
+) => {
     return (
-        <Button color="inherit" style={{ textTransform: "none" }} onClick={props.onClick}>
+        <Button
+            color="inherit"
+            style={{ textTransform: "none" }}
+            onClick={props.onClick}
+        >
             <Typography>{props.text}</Typography>
         </Button>
     );
-}
+};
 const NavBar: React.FC = () => {
     const router = useRouter();
     const { user } = useContext(CurrentUserContext);
@@ -22,20 +28,39 @@ const NavBar: React.FC = () => {
     const options: NavBarButtonProps[] = [
         { text: "Experiments", onClick: () => router.push("/experiment-list") },
         { text: "Assay Agenda", onClick: () => router.push("/agenda") },
-        { text: "Users", onClick: () => router.push("/users"), hidden: !user?.isAdmin },
-        { text : "Change Password", onClick: () => router.push("/auth/updatePassword")},
-        { text : "Sign Out", onClick: () => router.push("/auth/signOut")}
-    ]
-    
+        {
+            text: "Users",
+            onClick: () => router.push("/users"),
+            hidden: !user?.isAdmin,
+        },
+        {
+            text: "Change Password",
+            onClick: () => router.push("/auth/updatePassword"),
+        },
+        { text: "Sign Out", onClick: () => router.push("/auth/signOut") },
+    ];
+
     return (
         <AppBar position="sticky" color="primary">
-            <Toolbar >
+            <Toolbar>
                 <Typography variant="h6" style={{ flexGrow: 1 }}>
-                    Shelf Stability System
+                    <Link
+                        href="/experiment-list"
+                        style={{ color: "inherit", textDecoration: "none" }}
+                    >
+                        Shelf Stability System
+                    </Link>
                 </Typography>
 
-                {options.filter(option => !option.hidden).map((option, index) => <NavBarButton key={index} text={option.text} onClick={option.onClick} />)}
-
+                {options
+                    .filter((option) => !option.hidden)
+                    .map((option, index) => (
+                        <NavBarButton
+                            key={index}
+                            text={option.text}
+                            onClick={option.onClick}
+                        />
+                    ))}
             </Toolbar>
         </AppBar>
     );
