1f488ea9cfd0563725cecc5f9338413ce6a5ab2a
Keith Cressman <keith.cressman@duke.edu>
2024-04-08 15:08:13 -0400

    cleaned up util report

--- components/agenda/utilReportModal.tsx
+++ components/agenda/utilReportModal.tsx
@@ -7,11 +7,13 @@ import { LocalDate } from "@js-joda/core";
 import { fetchUtilizationData } from "@/lib/controllers/assayController";
 import { UtilizationReportRow } from "@/lib/controllers/types";
 import { generateExcelUtilizationReport } from "@/lib/generateExcelUtilizationReport";
+import { useGenerateLabUtilReport } from "@/lib/hooks/useGenerateLabUtilReport";
 
 export const UtilReportModal : React.FC = () => {
     const {isChoosingDates, setIsChoosingDates} = useContext(UtilizationReportContext);
     const [startDate, setStartDate] = useState<LocalDate>(LocalDate.now());
     const [endDate, setEndDate] = useState<LocalDate>(LocalDate.now());
+    const {mutate : fetchUtilizationDataAndGenerateExcelReport} = useGenerateLabUtilReport();
     const canSubmit = endDate.compareTo(startDate) > 0;
     return (
         <CloseableModal open={isChoosingDates} closeFn={() => setIsChoosingDates(false)} title="Lab Utilization Report">
@@ -36,13 +38,7 @@ export const UtilReportModal : React.FC = () => {
                         End date must come after the start date
                     </Typography>
                 }
-                <Button disabled={!canSubmit} variant="contained" color="primary" style={{textTransform : "none"}} onClick={() => {
-                    fetchUtilizationData({startDate, endDate}).then((rows : UtilizationReportRow[]) => {
-                        generateExcelUtilizationReport(rows, startDate, endDate);
-                    }).catch((error) => {
-                        console.log(error);
-                    })
-                }}>
+                <Button disabled={!canSubmit} variant="contained" color="primary" style={{textTransform : "none"}} onClick={() => fetchUtilizationDataAndGenerateExcelReport({startDate, endDate})}>
                     Generate Excel Report
                 </Button>
             </Stack>
